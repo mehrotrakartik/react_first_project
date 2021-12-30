933bcc50d0be47ccc4b3e36dc8e1bee1d4e1d6bb
Kartik Mehrotra <kartik.mehrotra@sap.com>
2021-12-30 13:11:11 +0530

    state intro

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import react from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 
 const App = () => {
   return (
     <div className='App'>     
-      <Welcome name="Kartik Mehrotra" />
+      <Message />
     </div>
   );
 }
