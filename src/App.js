f3fbf888328e294beb0b025d83238864548575d7
Kartik Mehrotra <kartik.mehrotra@sap.com>
2021-12-30 22:16:06 +0530

    react learning

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 
 const App = () => {
   return (
-    <div className='App'>     
-      <Message />
+    <div className='App'>
+      <ClassClick />
+      <FunctionClick />
     </div>
   );
 }
